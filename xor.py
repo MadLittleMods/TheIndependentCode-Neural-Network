a330d8983f77b76c7f30297f536920b9146a414b
Eric Eastwood <contact@ericeastwood.com>
2023-10-25 22:23:07 -0500

    Try out 2, 3, 3 layers

--- xor.py
+++ xor.py
@@ -9,12 +9,13 @@ from losses import mse, mse_prime
 from network import train, predict
 
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
-Y = np.reshape([[1, 0], [0, 1], [0, 1], [1, 0]], (4, 2, 1))
+# Y = np.reshape([[1, 0], [0, 1], [0, 1], [1, 0]], (4, 2, 1))
+Y = np.reshape([[1, 0, 0], [0, 1, 0], [0, 1, 0], [1, 0, 0]], (4, 3, 1))
 
 network = [
     Dense(2, 3),
     Sigmoid(),
-    Dense(3, 2),
+    Dense(3, 3),
     Softmax()
 ]
 
@@ -38,11 +39,19 @@ network[0].bias = np.array([
 network[2].weights = np.array([
     [2.6545684575714984e-01, 8.636176515580903e-01, 2.953281182408529e-01],
     [1.1489972410202928e-02, -1.0382172576148672e+00, 2.655416761236997e-01],
+    [4.8858481453740094e-01, 4.2309380092781346e-01, 1.0127531494214492e+00],
 ])
 network[2].bias = np.array([
     [1.0e-01],
     [1.0e-01],
+    [1.0e-01],
 ])
+
+for layer in network:
+    if hasattr(layer, 'weights'):
+        logging.error("layer.weights %s %s", layer.weights.shape, layer.weights)
+    if hasattr(layer, 'bias'):
+        logging.error("layer.bias %s %s", layer.bias.shape, layer.bias)
 
 # train
 train(network, mse, mse_prime, X, Y, epochs=1, learning_rate=0.1)
